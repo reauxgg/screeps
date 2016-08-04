c51f31e9335a0a567bdd08c9ffb5e8402c82c417
dsmythia <daniel_smythia@dell.com>
2016-08-04 16:28:54 -0500

    Basic modular support with just harvesters. Targetting simulator

--- Harvester.js
+++ Harvester.js
@@ -1,9 +1,116 @@
 var HarvCB = require('CreepBase');
 var HarvRL = require('RoomLevels');
-var HarvBody = {};
+var HarvBody = {
+    Level1 : [  WORK,
+                CARRY,
+                MOVE];
+    Level2 : [  WORK,WORK,
+                CARRY,CARRY,
+                MOVE,MOVE];
+    Level3 : [  WORK,WORK,WORK,
+                CARRY,CARRY,CARRY,
+                MOVE,MOVE,MOVE];
+    Level4 : [  WORK,WORK,WORK,
+                CARRY,CARRY,CARRY,CARRY,CARRY,
+                MOVE,MOVE,MOVE,MOVE];
+    Level5 : [  WORK,WORK,WORK,WORK,
+                CARRY,CARRY,CARRY,CARRY,CARRY,
+                MOVE,MOVE,MOVE,MOVE,MOVE];
+    Level6 : [  WORK,WORK,WORK,WORK,
+                CARRY,CARRY,CARRY,CARRY,CARRY,
+                MOVE,MOVE,MOVE,MOVE,MOVE];
+};
+
 
 var Harvester = {
-        
+    GetBody : function(level) {
+        if (level < HarvRL.EnergyLevels.Level1)
+        {
+            return HarvBody.Level1;
+        }
+        if (level < HarvRL.EnergyLevels.Level2)
+        {
+            return HarvBody.Level2;
+        }
+        if (level < HarvRL.EnergyLevels.Level3)
+        {
+            return HarvBody.Level3;
+        }
+        if (level < HarvRL.EnergyLevels.Level4)
+        {
+            return HarvBody.Level4;
+        }
+        if (level < HarvRL.EnergyLevels.Level5)
+        {
+            return HarvBody.Level5;
+        }
+        if (level < HarvRL.EnergyLevels.Level6)
+        {
+            return HarvBody.Level6;
+        }
+    }
+
+    Run : function(Creep)
+    {
+        if (Creep.carry.energy == Creep.carryCapacity)
+        {
+            Creep.memory.gather = false;
+        }
+        else if (Creep.carry.energy < 50)
+        {
+            Creep.memory.gather = true;
+        }
+
+        if (Creep.memory.gather)
+        {
+            var target = Creep.pos.findClosestByPath(FIND_DROPPED_ENERGY);
+            if (target)
+            {
+                if (Creep.pickup(target) == ERR_NOT_IN_RANGE)
+                {
+                    Creep.moveTo(target);
+                }
+            }
+            else {
+                target = Creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                if (target)
+                {
+                    if (Creep.harvest(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                    {
+                        Creep.moveTo(target);
+                    }
+                }
+            }
+        }
+        else
+        {
+            var target = Creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter : obj => {
+                        return  ((obj.structureType == STRUCTURE_SPAWN) ||
+                                (obj.structureType == STRUCTURE_EXTENSION)) &&
+                                (obj.energy < obj.energyCapacity);
+                    }
+                });
+            if (!target)
+            {
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                        filter : obj => {
+                            return (obj.structureType == STRUCTURE_TOWER) &&
+                                    (obj.energy < obj.energyCapacity);
+                        }
+                    });
+                if (!target)
+                {
+                    target = Creep.room.storage;
+                }
+            }
+            if (Creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            {
+                Creep.moveTo(target);
+            }
+        }
+    }
+
 };
 
 module.exports = Harvester;
--- main.js
+++ main.js
@@ -6,15 +6,13 @@ var hCap = 4;
 var bCap = 1;
 var rCap = 1;
 var uCap = 3;
-var Bodies = [];
-
 
 var body = [WORK,CARRY,MOVE];
 //var body = [WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];
 //var body = [WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE];
 var healer = [HEAL,HEAL,MOVE,MOVE,MOVE,MOVE];
 var fighter = [RANGED_ATTACK, RANGED_ATTACK, MOVE,MOVE,MOVE,MOVE];
-
+var CreepCount = 0;
 //The real stuff starts here.
 module.exports.loop = function () {
     for (var name in Memory.creeps) {
@@ -29,14 +27,47 @@ module.exports.loop = function () {
         var MyRoom = Game.rooms[Name];
         var TotalEnergy = Room.energyAvailable;
         var MaxEnergy = Room.energyCapacityAvailable;
-        var Population = 0;
         var RoomCreeps = MyRoom.find(FIND_MY_CREEPS);
+        var Population = RoomCreeps.length;
+        var HarvPop = 0;
+        var Unknown = 0;
 
+        for (var Creep in RoomCreeps)
+        {
+            switch(Creep.memory.Role)
+            {
+                case CreepBase.Harvest:
+                    HarvPop += 1;
+                    break;
+                default:
+                    Unknown += 1;
+            }
+            Harvester.Run(Creep);
+        }
+
+        if (HarvPop < hCap)
+        {
+            var NewBody = Harvester.GetBody(MyRoom.energyCapacityAvailable);
+            for (var Spawn in MyRoom.find(FIND_MY_SPAWNS))
+            {
+                if (!Spawn.spawning)
+                {
+                    var result = Spawn.createCreep(NewBody, "H" + CreepCount, {Role: CreepBase.Harvest});
+                    if (_.isString(result))
+                    {
+                        console.log("Built new Harvester, " + result);
+                        CreepCount++;
+                        break;
+                    }
+                }
+            }
+        }
 
 
         //These need to be adjusted at the start of a room to WORK,CARRY,MOVE
-        console.log('-------------------------')
-
+        console.log('-------------------------');
+        console.log(' Harvesters: ' + HarvPop + " of " + hCap);
+        console.log(' Energy: ' + TotalEnergy + " of " + MaxEnergy);
 
     }
 
